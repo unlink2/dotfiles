9781f71cc2fc573ad048243176134b57523feba0
Lukas Krickl <lukas@krickl.dev>
2023-07-27 06:46:24 +0200

    Added alias script

---
+++ alias.zsh
@@ -0,0 +1,38 @@
+# some shortcuts
+alias cp="cp -i"                          # confirm before overwriting something
+alias df='df -h'                          # human-readable sizes
+alias free='free -m'                      # show sizes in MB
+alias np='nano -w PKGBUILD'
+alias more=less
+
+alias ls='ls --color=auto'
+alias grep='grep --colour=auto'
+alias egrep='egrep --colour=auto'
+alias fgrep='fgrep --colour=auto'
+
+alias ls="lsd"
+alias ll='ls -l'
+alias la='ls -a'
+alias hx='helix'
+alias vim='nvim'
+
+alias gs='git status'
+alias gl='git log'
+alias gco='git add ./ && git commit -a'
+alias gce='git checkout'
+alias gbr='git branch'
+
+# mji commit command shortcut
+alias mjc='mji --commit'
+
+alias cb='cargo build'
+alias cr='cargo run'
+alias ct='cargo test'
+alias ci='cargo install'
+alias cw='cargo watch'
+
+alias cat='bat'
+alias zel='zellij'
+
+alias ssh='TERM=xterm-256color ssh'
+
